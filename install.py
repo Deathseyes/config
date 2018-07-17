4c3c0cbdedc36836f8151f17ce1cc549111a0f6c
nijisou <375153450@qq.com>
2018-07-17 15:59:38 +0800

    add config
    修改rime切换输入法的快捷键

--- install.py
+++ install.py
@@ -7,10 +7,12 @@ import shutil
 fileList = [
     'wubi86.dict.yaml',
     'wubi86.custom.yaml',
+    'default.custom.yaml',
 ]
 
 targetPath = os.environ['APPDATA'] + os.sep + 'Rime' + os.sep
 for fileName in fileList:
+    print('install: ' + fileName)
     shutil.copy('./' + fileName, targetPath + fileName)
 
 input('press any key to exit.')
